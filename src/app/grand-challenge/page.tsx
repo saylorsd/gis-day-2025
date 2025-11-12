c997f389a1dae53b4d46886422f6d51bd83549f6
Steve Saylor <steven.saylor@pitt.edu>
2025-11-12 10:40:18 -0500

    fix challenge page style

--- src/app/grand-challenge/page.tsx
+++ src/app/grand-challenge/page.tsx
@@ -4,62 +4,62 @@ export default function GrandChallengePage() {
       <h1 className="text-6xl">
         Grand Challenge Seminar Expo: Working with County Health Data
       </h1>
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Investigating High School Completion Percentage and Income Inequality in
         New York
       </div>
       <p className="text-base">Sai Pendekanti, Jackie Sun, Yash Kshirsagar</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Infant Mortality Race Disparity
       </div>
       <p className="text-base">
         Alexis Choi, Sophie Pan, Vikram Oberai, Walter Hsieh
       </p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Visualizing Income’s Effect on Food Insecurity
       </div>
       <p className="text-base">Adam Zhu, Brian Lam, Aidan Chan, Kavya Tomar</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Pennsylvania’s Sleep Crisis
       </div>
       <p className="text-base">
         Sahana Chaudhuri, Siri Ganapathineedi, Max Wen
       </p>
 
-      <div className="mt-4 font-display text-2xl">Rainy Days, Blue Moods</div>
+      <div className="mt-12 font-display text-2xl">Rainy Days, Blue Moods</div>
       <p className="text-base">Abigail Hilton, Nathan Ye, Sienna Yan</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Environmental Factors and Associations with Mental Health in PA
       </div>
       <p className="text-base">Daniel Burnashev, Darren Huang, Shane Lee</p>
 
-      <div className="mt-4 font-display text-2xl">NO SLEEP, MORE BEEPS!</div>
+      <div className="mt-12 font-display text-2xl">NO SLEEP, MORE BEEPS!</div>
       <p className="text-base">
         Eudora Deng, Dharan Kwan, &amp; Isha Swaminathan
       </p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Relationship Between State Uninsured Rates and Motor Vehicle Crash
         Mortality
       </div>
       <p className="text-base">Jett Chen, Wellesley Li, Zachary Xiao</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Analysis of Mental Health Correlations in California
       </div>
       <p className="text-base">Sydney Montgomery, Max Gu, Parker Merritt</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Relationship Between Household Income and High School Graduation Rates
         in California
       </div>
       <p className="text-base">Justin Tan, Meera Rajendran, Max Zou</p>
 
-      <div className="mt-4 font-display text-2xl">
+      <div className="mt-12 font-display text-2xl">
         Median Income and Drug Overdose Deaths
       </div>
       <p className="text-base">Danielle Liberman, Brady Quo, Keynes Wu</p>
